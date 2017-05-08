d2dd3e559ebb6cb8e093a804d3a6bf5cadb7e53d
soachishti <soachishti@outlook.com>
2017-05-08 17:46:32 +0500

    Added host for local network

--- doctur/settings.py
+++ doctur/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'fb#if-%e!j&1ykrnmt%^jis*=n9)v_l#8@a*=#a!96f#j-a8q5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.201']
 
 
 # Application definition
